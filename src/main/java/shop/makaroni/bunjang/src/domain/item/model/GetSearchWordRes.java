43d973fe6e3f96c73023c2e32b114f5a55bc2c37
SW-H <hsw08@naver.com>
2022-08-27 12:04:19 +0900

    feat : 검색 키워드와 연관 키워드 조회

---
+++ src/main/java/shop/makaroni/bunjang/src/domain/item/model/GetSearchWordRes.java
@@ -0,0 +1,12 @@
+package shop.makaroni.bunjang.src.domain.item.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GetSearchWordRes {
+
+}
