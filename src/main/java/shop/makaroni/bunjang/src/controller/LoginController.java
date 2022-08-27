4f777a49552fa81446e17df158d27a8485674d86
kookiedncream <kim76744@naver.com>
2022-08-28 00:52:13 +0900

    feat: 카카오 로그인 토큰 받아오기 구현

--- src/main/java/shop/makaroni/bunjang/src/controller/LoginController.java
+++ src/main/java/shop/makaroni/bunjang/src/controller/LoginController.java
@@ -1,29 +1,46 @@
 package shop.makaroni.bunjang.src.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import shop.makaroni.bunjang.src.domain.login.LoginRequest;
 import shop.makaroni.bunjang.src.response.ResponseInfoWithJwt;
+import shop.makaroni.bunjang.src.service.KakaoService;
 import shop.makaroni.bunjang.src.service.LoginService;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Map;
 
 import static shop.makaroni.bunjang.src.response.SuccessStatus.LOGIN_SUCCESS;
 
+@Slf4j
 @RestController
 @RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
 
 	private final LoginService loginService;
+	private final KakaoService kakaoService;
 
 	@PostMapping
 	public ResponseEntity<ResponseInfoWithJwt> login(@Valid @RequestBody LoginRequest loginRequest) {
 		String jwt = loginService.login(loginRequest);
 		return ResponseEntity.ok(ResponseInfoWithJwt.of(LOGIN_SUCCESS, jwt));
+	}
+
+	@GetMapping("/kakao")
+	public ResponseEntity<Void> getCI(@RequestParam String code) throws IOException {
+
+		log.info("kakao Code = {} ", code);
+		String access_token = kakaoService.getToken(code);
+		Map<String, Object> userInfo = kakaoService.getUserInfo(access_token);
+		return ResponseEntity.noContent().build();
 	}
 }
