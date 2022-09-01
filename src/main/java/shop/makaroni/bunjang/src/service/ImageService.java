2bf0c7a93a43e9eb5664c1f346420886ac819c70
SW-H <hsw08@naver.com>
2022-09-02 07:25:35 +0900

    fix : transaction 처리 추가

--- src/main/java/shop/makaroni/bunjang/src/service/ImageService.java
+++ src/main/java/shop/makaroni/bunjang/src/service/ImageService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import shop.makaroni.bunjang.config.BaseException;
 import shop.makaroni.bunjang.src.dao.ItemDao;
@@ -24,6 +25,7 @@ import static shop.makaroni.bunjang.config.BaseResponseStatus.*;
 @Slf4j
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class ImageService {
 
     private final AmazonS3Client amazonS3Client;
